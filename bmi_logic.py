de76d202c3775f4a8306670a17d89533145cf105
masu <118078272+MaSu-007@users.noreply.github.com>
2025-06-06 14:11:33 +0400

    Update bmi_logic.py

--- bmi_logic.py
+++ bmi_logic.py
@@ -1,12 +1,12 @@
 class BMICalculator:
     def __init__(self, height_cm, weight_kg):
-        self.height_cm = height_cm / 100
+        self.height_m = height_cm / 100
         self.weight = weight_kg
 
     def calculate_bmi(self):
-        if self.height_cm <= 0:
+        if self.height_m <= 0:
             return None
-        bmi = self.weight / (self.height_cm ** 2)
+        bmi = self.weight / (self.height_m ** 2)
         return round(bmi, 2)
 
     def interpret_bmi(self, bmi):
